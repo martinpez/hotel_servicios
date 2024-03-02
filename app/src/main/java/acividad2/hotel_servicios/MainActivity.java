c9af9f283c558d20b07399cd2c42397b9f5a7e0a
martinpez <perezmercadomartin@gmail.com>
2024-03-02 13:03:50 -0500

    Initial commit

---
+++ app/src/main/java/acividad2/hotel_servicios/MainActivity.java
@@ -0,0 +1,14 @@
+package acividad2.hotel_servicios;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
